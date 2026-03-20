4ff434ead1af68121089653c4d771d90a6d05e2c
Santiago Hernandez <santiagohernanandez5@gmail.com>
2026-03-19 22:10:14 -0500

    Correccion vercel

--- server.js
+++ server.js
@@ -2,10 +2,14 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 
 app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
