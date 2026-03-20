d97121441120db1a4150280438a5e4017f366380
Santiago Hernandez <santiagohernanandez5@gmail.com>
2026-03-19 22:11:48 -0500

    correccion vercel

---
+++ api/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, '../public')));
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public', 'index.html'));
+});
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public', 'index.html'));
+});
+
+module.exports = app;
